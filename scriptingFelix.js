b30eb4266d7bff0136f07483efe022f075b9f563
FelixvL <digitaleservice@gmail.com>
2021-11-04 16:11:04 +0100

    bestelling overzicht en naam toevoegen

---
+++ scriptingFelix.js
@@ -0,0 +1,30 @@
+function mijnfunctie(){
+    let vanmij = new XMLHttpRequest();
+    vanmij.onreadystatechange = function(){
+        if(this.readyState == 4){
+            console.log(this.responseText);
+            var hetantwoord = JSON.parse(this.responseText);
+            document.getElementById("uitkomst").innerHTML = hetantwoord[0];
+
+        }
+    }
+    vanmij.open("GET","https://localhost:44344/api/Ali",true);
+    vanmij.send();
+}
+function alleProductenTonenInTabel(){
+    let vanmij = new XMLHttpRequest();
+    vanmij.onreadystatechange = function(){
+        if(this.readyState == 4){
+            console.log(this.responseText);
+            var arrayMetProducten = JSON.parse(this.responseText);
+            var tabelelem = document.getElementById("productenTabel");
+            tabelelem.innerHTML = "";
+            tabelelem.innerHTML += "<tr><th>naam</th><th>prijs</th><th>omschrijving</th></tr>";
+            for( var i = 0 ; i < arrayMetProducten.length ; i++  ){
+                tabelelem.innerHTML += " <tr><td>"+ arrayMetProducten[i].naam +"</td><td>"+ arrayMetProducten[i].prijs +"</td><td>"+ arrayMetProducten[i].specificaties +"</td></tr>";
+            }
+        }
+    }
+    vanmij.open("GET","https://localhost:44344/api/Ali/LijstProducten",true);
+    vanmij.send();
+}
