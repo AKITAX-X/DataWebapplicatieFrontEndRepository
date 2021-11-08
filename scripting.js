4f36f0b37207147ab8a4f1a48fc20097bfa0833a
AKITAX-X <muhammetali.esenkaya@hotmail.com>
2021-11-08 09:02:45 +0100

    aanpassing

--- scripting.js
+++ scripting.js
@@ -64,20 +64,14 @@
 // }
 
 
-function voegProductToe(event){
-
-    
-    event.preventDefault();
-    console.log(event);
-    const elements = event.target.elements
+function voegProductToe()
+{
     let productObj = {};
-
-    productObj.naam = elements.productNaamInvoer.value;
-    productObj.prijs = parseFloat(elements.productPrijsInvoer.value);
-    productObj.specificaties = elements.productSpecsInvoer.value;
-    
+    productObj.naam = document.getElementById("productNaamInvoer").value;
+    productObj.prijs = parseFloat(document.getElementById("productPrijsInvoer").value);
+    productObj.specificaties = document.getElementById("productSpecsInvoer").value;
     let productJSON = JSON.stringify(productObj);
-    let vanali = new XMLHttpRequest(); 
+    let vanali = new XMLHttpRequest();
     vanali.onreadystatechange = function(){
         if(this.readyState == 4){
             console.log("terug van voeg product toe");
@@ -85,18 +79,8 @@ function voegProductToe(event){
     }    
     vanali.open("POST","https://localhost:44344/api/Ali/voegProductToe",true);
     vanali.setRequestHeader("Content-Type","application/json");
-    console.log(productJSON)
     vanali.send(productJSON);
 }
-
-function deleteRow(event){
-    const element = event.target
-    const id = element.getAttribute("data-id")
-    console.log(id)
-
-}
-
-
 
 
 function toonAlleProducten(){
@@ -107,28 +91,16 @@ function toonAlleProducten(){
         var arrayMetProducten= JSON.parse(this.responseText);
         var tabelelem = document.getElementById("tabelProducten");
 
-        tabelelem.innerHTML = "<tr><th>naam</th><th>prijs</th><th>omschrijving</th><th></th></tr>";
-        for(let i = 0; i < arrayMetProducten.length; i++ )
+        tabelelem.innerHTML = "<tr><th>naam</th><th>prijs</th><th>omschrijving</th></tr>";
+        for(var i = 0; i < arrayMetProducten.length; i++ )
 
         {
-            tabelelem.innerHTML += `
-                <tr>
-                    <td>${arrayMetProducten[i].naam}</td>
-                    <td>${arrayMetProducten[i].prijs}</td>
-                    <td>${arrayMetProducten[i].specificaties}</td>
-                    <td><button data-id="${arrayMetProducten[i].id}" id="rowButton-${arrayMetProducten[i].id}">Delete user</button></td>
-                </tr>
-            `;
+            tabelelem.innerHTML += "<tr><td>"+arrayMetProducten[i].naam + "</td><td>"+arrayMetProducten[i].prijs + "</td><td>"+arrayMetProducten[i].specificaties + "</td></tr>";
 
-            
         }
+        
 
-        for(let i = 0; i < arrayMetProducten.length; i++ )
-        {
-            const buttonSelector = document.getElementById(`rowButton-${arrayMetProducten[i].id}`)
-            buttonSelector.addEventListener("click", deleteRow)
-            console.log(buttonSelector)
-        }
+
         // tabelelem.innerHTML = "";
         // tabelelem.innerHTML += "<tr><th>naam</th><th>prijs</th><th>omschrijving</th></tr>";
         // for( var i = 0 ; i < arrayMetProducten.length ; i++  ){
@@ -139,10 +111,3 @@ function toonAlleProducten(){
 vanali.open("GET","https://localhost:44344/api/Ali/alleProducten",true);
 vanali.send();
 }
-
-
-const form = document.getElementById("productForm")
-form.addEventListener("submit", voegProductToe)
-
-
-
