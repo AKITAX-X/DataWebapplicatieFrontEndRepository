662c2892c42faffd568b0fef354daae9cba8c8d3
AKITAX-X <muhammetali.esenkaya@hotmail.com>
2021-11-18 13:53:05 +0100

    bestelling pagina aangepast

---
+++ JS/bestelling.js
@@ -0,0 +1,18 @@
+productids = [];
+        window.onload = function () {
+            var bestellingproductids = JSON.parse(localStorage.getItem("geheimesleutelvancheapswinkel"));
+            const urlParams = new URLSearchParams(window.location.search);
+            const productid = urlParams.get('productid');
+            bestellingproductids.push(productid);
+            localStorage.setItem("geheimesleutelvancheapswinkel", JSON.stringify(bestellingproductids));
+            productids = bestellingproductids;
+            toonbestelling();
+        }
+        function toonbestelling(){
+            var overz = document.getElementById("overzicht");
+            for(var x =0 ; x < productids.length; x++){
+                overz.innerHTML += ">>"+productids[x]+"<br>";
+            }
+        }
+
+    
